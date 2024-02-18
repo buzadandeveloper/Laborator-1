69069634fe442c4fdc1cf7fb70a0b2b2685dbb2a
Buza Dan <buzadandeveloper@gmail.com>
2024-02-18 15:53:52 +0200

    new changes

--- JS/timeLoader.js
+++ JS/timeLoader.js
@@ -17,5 +17,19 @@ function timeLoaded(){
     let spanTime = document.createElement("span");
     spanTime.textContent = timeString;
     divInfoNews.append(spanTime);
+
+    let spanTime2 = document.createElement("span");
+    spanTime2.textContent = timeString;
+    divInfoText.append(spanTime2);
+
+    let spanTime3 = document.createElement("span");
+    spanTime3.textContent = timeString;
+    divInfoText2.append(spanTime3);
+
+    let spanTime4 = document.createElement("span");
+    spanTime4.textContent = timeString;
+    divInfoText3.append(spanTime4);
+
+    
 }
 document.addEventListener("DOMContentLoaded", timeLoaded);
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Main News Container
 let newsMain = document.querySelector(".container-main");
 
 let imgNewsMain = document.createElement("img");
@@ -22,7 +23,8 @@ let h1InfoNews = document.createElement("h1");
 h1InfoNews.textContent = "Neuralink a realizat primul implant în creierul uman";
 
 let pInfoNews = document.createElement("p");
-pInfoNews.textContent = "Pe 28 ianuarie, compania de neurotehnologie Neuralink a antreprenorului Elon Musk a realizat pentru prima dată un implant în creierul uman.";
+pInfoNews.textContent =
+  "Pe 28 ianuarie, compania de neurotehnologie Neuralink a antreprenorului Elon Musk a realizat pentru prima dată un implant în creierul uman.";
 
 aInfoNews.append(h1InfoNews);
 
@@ -31,4 +33,87 @@ divInfoNews.append(h1InfoNews);
 divInfoNews.append(pInfoNews);
 newsMain.append(divInfoNews);
 
-console.log(newsMain);
+// Second News Container
+let newsSecond = document.querySelector(".container-second");
+
+let divSecondNews = document.createElement("div");
+divSecondNews.classList.add("news");
+
+let aImgSecondNews = document.createElement("a");
+aImgSecondNews.setAttribute("href", "#");
+
+let imgSecondNews = document.createElement("img");
+imgSecondNews.setAttribute("src", "/image/apps-img.jpg");
+imgSecondNews.setAttribute("alt", "apps-img");
+
+let divInfoText = document.createElement("div");
+divInfoText.classList.add("info-text");
+
+let h2Title = document.createElement("h2");
+h2Title.textContent = "Ce funcție obligatorie WhatsApp are de implementat";
+
+aImgSecondNews.append(imgSecondNews);
+
+divSecondNews.append(aImgSecondNews);
+divInfoText.append(h2Title);
+divInfoText.append(h2Title);
+divSecondNews.append(divInfoText);
+
+newsSecond.append(divSecondNews);
+// --------------------------------------------------------------------
+newsSecond = document.querySelector(".container-second");
+
+divSecondNews = document.createElement("div");
+divSecondNews.classList.add("news");
+
+aImgSecondNews = document.createElement("a");
+aImgSecondNews.setAttribute("href", "#");
+
+imgSecondNews = document.createElement("img");
+imgSecondNews.setAttribute("src", "/image/chat-gpt-memory.jpg");
+imgSecondNews.setAttribute("alt", "apps-img");
+
+let divInfoText2 = document.createElement("div");
+divInfoText2.classList.add("info-text");
+
+h2Title = document.createElement("h2");
+h2Title.textContent = "În curând, ChatGPT ar putea memora date şi despre utilizatorii săi";
+
+aImgSecondNews.append(imgSecondNews);
+
+divSecondNews.append(aImgSecondNews);
+divInfoText2.append(h2Title);
+divInfoText2.append(h2Title);
+divSecondNews.append(divInfoText2);
+
+newsSecond.append(divSecondNews);
+//-------------------------------------------------------------------------
+newsSecond = document.querySelector(".container-second");
+
+divSecondNews = document.createElement("div");
+divSecondNews.classList.add("news");
+
+aImgSecondNews = document.createElement("a");
+aImgSecondNews.setAttribute("href", "#");
+
+imgSecondNews = document.createElement("img");
+imgSecondNews.setAttribute("src", "/image/chat-gpt2.jpg");
+imgSecondNews.setAttribute("alt", "apps-img");
+
+let divInfoText3 = document.createElement("div");
+divInfoText3.classList.add("info-text");
+
+h2Title = document.createElement("h2");
+h2Title.textContent = "Sondaj: Ce cred moldovenii despre inteligența artificială";
+
+aImgSecondNews.append(imgSecondNews);
+
+divSecondNews.append(aImgSecondNews);
+divInfoText3.append(h2Title);
+divInfoText3.append(h2Title);
+divSecondNews.append(divInfoText3);
+
+newsSecond.append(divSecondNews);
+
+
+console.log(newsSecond);
