7fbc82ffa2262e34cbada700f405c6e81931b3e6
Buza Dan <buzadandeveloper@gmail.com>
2024-02-27 10:45:02 +0200

    ch

--- HTML/Articles/MainArticle/main-article.js
+++ HTML/Articles/MainArticle/main-article.js
@@ -1,29 +1,34 @@
 let newsList = document.querySelector(".news-container");
 let divNews = document.createElement("div");
 divNews.classList.add("news");
-let aNews = document.createElement("a");
-aNews.setAttribute("href", "#");
 let imgNews = document.createElement("img");
 imgNews.setAttribute("src", "/image/neural-link.jpg");
 imgNews.setAttribute("alt", "brain-chip-img");
-aNews.append(imgNews);
+divNews.addEventListener("click", function(){
+    window.location.href = "#";
+});
+
 let h3News = document.createElement("h3");
 h3News.textContent = "Neuralink a realizat primul implant în creierul uman";
-divNews.append(aNews);
+divNews.append(imgNews);
 divNews.append(h3News);
 newsList.prepend(divNews);
-
+//--------------------------------------------------------------------------------------------
 divNews = document.createElement("div");
 divNews.classList.add("news");
-aNews = document.createElement("a");
-aNews.setAttribute("href", "#");
+
+divNews.addEventListener("click", function(){
+    window.location.href = "#";
+});
+
 imgNews = document.createElement("img");
-imgNews.setAttribute("src", "/image/chat-gpt.jpg");
+imgNews.setAttribute("src", "/image/chat-gpt2.jpg");
 imgNews.setAttribute("alt", "chat-gpt-img");
-aNews.append(imgNews);
+imgNews.setAttribute("href", "#");
+
 h3News = document.createElement("h3");
 h3News.textContent = "Sondaj: Ce cred moldovenii despre inteligența artificială";
-divNews.append(aNews);
+divNews.append(imgNews);
 divNews.append(h3News);
 newsList.append(divNews);
 
