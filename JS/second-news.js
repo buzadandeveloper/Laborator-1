c5a2139c5aab58df3540efe15cdff9bad8840bd5
Buza Dan <buzadandeveloper@gmail.com>
2024-03-04 17:53:31 +0200

    ch

--- JS/second-news.js
+++ JS/second-news.js
@@ -1,69 +1,35 @@
-// Second News Container
+import { toLink } from "./toLinks.js";
+
 let newsSecond = document.querySelector(".container-second");
 
-let divSecondNews = document.createElement("div");
-divSecondNews.classList.add("news");
+// Funcție pentru crearea și adăugarea de știri secundare
+function createSecondaryNews(id, imgPaths, altText, titleText) {
+  let divSecondNews = document.createElement("div");
+  divSecondNews.classList.add("news");
+  divSecondNews.setAttribute("id", id);
 
-let imgSecondNews = document.createElement("img");
-imgSecondNews.setAttribute("src", "/image/apps-img.jpg");
-imgSecondNews.setAttribute("alt", "apps-img");
+  let imgSecondNews = document.createElement("img");
+  imgSecondNews.setAttribute("src", imgPaths);
+  imgSecondNews.setAttribute("alt", altText);
 
-let divInfoText = document.createElement("div");
-divInfoText.setAttribute("id", "1");
-divInfoText.classList.add("info-text");
+  let divInfoText = document.createElement("div");
+  divInfoText.classList.add("info-text");
+  let h2Title = document.createElement("h2");
+  h2Title.textContent = titleText;
 
-let h2Title = document.createElement("h2");
-h2Title.textContent = "Ce funcție obligatorie WhatsApp are de implementat";
+  divSecondNews.append(imgSecondNews);
+  divInfoText.append(h2Title);
+  divSecondNews.append(divInfoText);
 
-divSecondNews.append(imgSecondNews);
-divInfoText.append(h2Title);
-divInfoText.append(h2Title);
-divSecondNews.append(divInfoText);
+  return divSecondNews;
+}
 
-newsSecond.append(divSecondNews);
-// --------------------------------------------------------------------
-// newsSecond = document.querySelector(".container-second");
+// Crearea și adăugarea știrilor secundare
+newsSecond.append(createSecondaryNews("1", "/image/apps-img.jpg", "apps-img", "Ce funcție obligatorie WhatsApp are de implementat"));
+newsSecond.append(createSecondaryNews("2", "/image/chat-gpt-memory.jpg", "chat-gpt-img","În curând, ChatGPT ar putea memora date despre utilizatorii săi"));
+newsSecond.append(createSecondaryNews("3", "/image/chat-gpt2.jpg", "chat-gpt-img","Sondaj: Ce cred moldovenii despre inteligența artificială"));
 
-divSecondNews = document.createElement("div");
-divSecondNews.classList.add("news");
-
-imgSecondNews = document.createElement("img");
-imgSecondNews.setAttribute("src", "/image/chat-gpt-memory.jpg");
-imgSecondNews.setAttribute("alt", "apps-img");
-
-let divInfoText2 = document.createElement("div");
-divInfoText2.setAttribute("id", "2");
-divInfoText2.classList.add("info-text");
-
-h2Title = document.createElement("h2");
-h2Title.textContent = "În curând, ChatGPT ar putea memora date despre utilizatorii săi";
-
-divSecondNews.append(imgSecondNews);
-divInfoText2.append(h2Title);
-divInfoText2.append(h2Title);
-divSecondNews.append(divInfoText2);
-
-newsSecond.append(divSecondNews);
-//-------------------------------------------------------------------------
-// newsSecond = document.querySelector(".container-second");
-
-divSecondNews = document.createElement("div");
-divSecondNews.classList.add("news");
-
-imgSecondNews = document.createElement("img");
-imgSecondNews.setAttribute("src", "/image/chat-gpt2.jpg");
-imgSecondNews.setAttribute("alt", "apps-img");
-
-let divInfoText3 = document.createElement("div");
-divInfoText3.setAttribute("id", "3");
-divInfoText3.classList.add("info-text");
-
-h2Title = document.createElement("h2");
-h2Title.textContent = "Sondaj: Ce cred moldovenii despre inteligența artificială";
-
-divSecondNews.append(imgSecondNews);
-divInfoText3.append(h2Title);
-divInfoText3.append(h2Title);
-divSecondNews.append(divInfoText3);
-
-newsSecond.append(divSecondNews);
+// Adăugarea linkurilor pentru fiecare știre secundară
+toLink("1", "#");
+toLink("2", "#");
+toLink("3", "#");
--- JS/third-news.js
+++ JS/third-news.js
@@ -3,6 +3,7 @@ let newsThird = document.querySelector(".container-third");
 
 let divThirdNews = document.createElement("div");
 divThirdNews.classList.add("news");
+divThirdNews.setAttribute("id", "4");
 
 let imgThirdNews = document.createElement("img");
 imgThirdNews.setAttribute("src", "/image/facebook.jpg");
@@ -11,7 +12,6 @@ imgThirdNews.setAttribute("alt", "facebook-img");
 divThirdNews.append(imgThirdNews);
 
 divInfoTextThird = document.createElement("div");
-divInfoTextThird.setAttribute("id", "4");
 divInfoTextThird.classList.add("info-text");
 
 let h3Title = document.createElement("h3");
@@ -24,6 +24,7 @@ divThirdNews.append(divInfoTextThird);
 
 let divThirdNews2 = document.createElement("div");
 divThirdNews2.classList.add("news");
+divThirdNews2.setAttribute("id", "5");
 
 imgThirdNews = document.createElement("img");
 imgThirdNews.setAttribute("src", "/image/elon-musk.jpg");
@@ -32,7 +33,6 @@ imgThirdNews.setAttribute("alt", "elon-musk-img");
 divThirdNews2.append(imgThirdNews);
 
 let divInfoTextThird2 = document.createElement("div");
-divInfoTextThird2.setAttribute("id", "5");
 divInfoTextThird2.classList.add("info-text");
 
 h3Title = document.createElement("h3");
@@ -45,6 +45,7 @@ divThirdNews2.append(divInfoTextThird2);
 
 let divThirdNews3 = document.createElement("div");
 divThirdNews3.classList.add("news");
+divThirdNews3.setAttribute("id", "6");
 
 imgThirdNews = document.createElement("img");
 imgThirdNews.setAttribute("src", "/image/nasa.jpg");
@@ -53,7 +54,6 @@ imgThirdNews.setAttribute("alt", "mars-img");
 divThirdNews3.append(imgThirdNews);
 
 let divInfoTextThird3 = document.createElement("div");
-divInfoTextThird3.setAttribute("id", "6");
 divInfoTextThird3.classList.add("info-text");
 
 h3Title = document.createElement("h3");
@@ -66,6 +66,7 @@ divThirdNews3.append(divInfoTextThird3);
 
 let divThirdNews4 = document.createElement("div");
 divThirdNews4.classList.add("news");
+divThirdNews4.setAttribute("id", "7");
 
 imgThirdNews = document.createElement("img");
 imgThirdNews.setAttribute("src", "/image/bill-gates.jpg");
@@ -74,7 +75,6 @@ imgThirdNews.setAttribute("alt", "bill-gates-img");
 divThirdNews4.append(imgThirdNews);
 
 let divInfoTextThird4 = document.createElement("div");
-divInfoTextThird4.setAttribute("id", "7");
 divInfoTextThird4.classList.add("info-text");
 
 h3Title = document.createElement("h3");
@@ -98,3 +98,7 @@ thirdTwo.append(divThirdNews4);
 newsThird.append(thirdOne);
 newsThird.append(thirdTwo);
 
+function createThirdNews(id, imgPaths, titleText){
+    let divThirdNews = document.createElement("div");
+    
+}
